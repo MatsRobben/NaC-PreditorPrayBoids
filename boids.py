91a2950a80f3419d03f94e1120806451d640f782
MatsRobben <matsrobben@gmail.com>
2024-06-13 10:48:40 +0200

    fix merge errors

--- boids.py
+++ boids.py
@@ -44,7 +44,6 @@ MARGIN_TOP = config['MARGIN_TOP']
 MARGIN_BOTTOM = config['MARGIN_BOTTOM']
 
 def add_newboid(parent, boids, classes, energies, boid_ids, next_boid_id, param_dict, params=None):
-def add_newboid(parent, boids, classes, energies, boid_ids, next_boid_id, param_dict, params=None):
     new_row = np.array(
         [np.random.uniform(0, WIDTH), np.random.uniform(0, HEIGHT), np.random.uniform(-1, 1), np.random.uniform(-1, 1)],
         dtype=np.float32)
@@ -70,14 +69,6 @@ def add_newboid(parent, boids, classes, energies, boid_ids, next_boid_id, param_
         # new_params = np.random.normal(loc=parent_params, scale=PARAM_DEVIATION, size=parent_params.shape)
         # params = np.append(params, new_params[None, ...], axis=0)
 
-        # Track new boid's parameters
-        param_dict[next_boid_id] = new_params
-
-    # Assign unique ID to the new boid
-    next_boid_id += 1
-    boid_ids = np.append(boid_ids, next_boid_id)
-
-    return boids, classes, energies, boid_ids, next_boid_id, params
         # Track new boid's parameters
         param_dict[next_boid_id] = new_params
 
@@ -318,7 +309,6 @@ def simulation(visual=True, sim_length=None):
                 random_factors = np.append(random_factors, np.random.randint(1, REPRODUCE_CYCLE[classes[parent]], 1))
 
         boids, classes, energies, random_factors, boid_ids, params = remove_boid(deleteableBoids, boids, classes, energies, random_factors, boid_ids, params=params)
-        boids, classes, energies, random_factors, boid_ids, params = remove_boid(deleteableBoids, boids, classes, energies, random_factors, boid_ids, params=params)
 
         if visual:
             draw_boids(screen, boids, classes)
@@ -337,7 +327,6 @@ def simulation(visual=True, sim_length=None):
         pygame.quit()
 
     return boid_counts, family_tree, param_dict
-    return boid_counts, family_tree, param_dict
 
 def plot_boid_counts(boid_counts, num_classes):
     class_names = ['Prey', 'Predator']
@@ -355,35 +344,6 @@ def plot_boid_counts(boid_counts, num_classes):
     plt.legend()
     plt.savefig(f'figures/boid_counts_{config_name}.png')
     plt.close()
-
-def plot_family_tree(param_dict, family_tree, param_index_pairs):
-    fig, axes = plt.subplots(len(param_index_pairs), figsize=(12, 8))
-    
-    if len(param_index_pairs) == 1:
-        axes = [axes]
-
-    for ax, (param_x, param_y) in zip(axes, param_index_pairs):
-        print(param_x, param_y)
-        # Plot the parameters for all boids
-        for boid_id, params in param_dict.items():
-            print(params)
-            break
-            # for class_idx in range(params.shape[1]):
-    #             ax.scatter(params[param_x, class_idx], params[param_y, class_idx], label=f'Class {class_idx}')
-        
-    #     # Draw parent-child lines
-    #     for parent_id, child_id in family_tree:
-    #         parent_params = param_dict[parent_id]
-    #         child_params = param_dict[child_id]
-    #         ax.plot([parent_params[param_x], child_params[param_x]], 
-    #                 [parent_params[param_y], child_params[param_y]], 'k-')
-        
-    #     ax.set_xlabel(f'Parameter {param_x}')
-    #     ax.set_ylabel(f'Parameter {param_y}')
-    #     ax.legend()
-    
-    plt.tight_layout()
-    plt.show()
 
 def save_simulation_data(filename, boid_counts, family_tree, param_dict):
     with open(filename, 'wb') as f:
